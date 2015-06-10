d5ee5b03b7d657d78892ee8b3f350fb1beb67be2
Dariusz Ciechanowski <dciechan94@gmail.com>
2015-06-10 20:13:01 +0200

    Added example components

--- src/swingApp/MainFrame.java
+++ src/swingApp/MainFrame.java
@@ -1,11 +1,36 @@
 package swingApp;
 
+import java.awt.BorderLayout;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 public class MainFrame extends JFrame {
+	
+	JTextField nameField;
+	JTextField occupationField;
+	JTextArea textArea;
+	JButton okBtn;
+	
 	public MainFrame() {
 		super("Hello World");
 		
+		nameField = new JTextField(10);
+		occupationField = new JTextField(10);
+		okBtn = new JButton("OK");
+		
+		textArea = new JTextArea();
+		
+		
+		setLayout(new BorderLayout());
+		
+		
+		add(nameField, BorderLayout.NORTH);
+		add(occupationField, BorderLayout.WEST);
+		add(textArea, BorderLayout.CENTER);
+		add(okBtn, BorderLayout.SOUTH);
 		
 		setSize(400, 300);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
